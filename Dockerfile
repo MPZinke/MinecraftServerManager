65cb1b416864ed211b0b7f570d938c8b86873cdc
mpzinke <mathew.zinke@gmail.com>
2026-06-14 22:01:23 -0500

    Implements typescript for JS

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,26 @@
+# FROM: https://hub.docker.com/_/rust/
+FROM node:23.5-slim AS builder
+
+RUN apt update
+RUN apt install -y git
+RUN git clone --depth 1 https://github.com/MPZinke/TypeScript-Jinja.git /Typescript-Jinja
+WORKDIR /Typescript-Jinja
+RUN npm install
+RUN npm run build
+# RUN chmod -R 777 /usr/Typescript-Jinja/built
+# RUN mkdir /tsc
+# COPY ./built /tsc/built
+
+COPY ./source /usr/source
+COPY ./tsconfig.json /usr/tsconfig.json
+WORKDIR /usr
+
+# RUN node /usr/Typescript-Jinja/built/local/tsc.js --build --verbose
+RUN node /Typescript-Jinja/built/local/tsc.js
+
+
+# ---------------------- #
+
 FROM python:3.13-slim
 
 RUN apt update
@@ -8,7 +31,7 @@ RUN pip3 install toml-cli
 COPY ./pyproject.toml ./
 RUN pip3 install $(toml get --toml-path pyproject.toml project.dependencies | sed -e "s/[']/\"/g" | jq -r '.[]')
 
-COPY ./source ./source
+COPY --from=builder /usr/source ./source
 
 EXPOSE 443
 
---
+++ source/webapp/Templates/worlds/world/index.ts
@@ -0,0 +1,113 @@
+
+const LAST_PLAYED_TD = document.getElementById("last_played-td")!;
+const ONLINE_PLAYERS_TD = document.getElementById("online_players-td")!;
+const RUN_BUTTON_DIV = document.getElementById("run_button-td")!;
+const RUNNING_CONTAINER_TD = document.getElementById("running_container-td")!;
+const STATE_TD = document.getElementById("state-td")!;
+
+
+async function get_online_players()
+{
+	let response = await fetch(
+		`/worlds/{{ world.id }}/players/online`,
+		{
+			method: `GET`,
+			headers: {"Content-Type": "application/json"},
+		}
+	);
+	let response_body = await response.text();
+
+	ONLINE_PLAYERS_TD.innerHTML = response_body;
+}
+
+
+async function update_state()
+{
+	let response = await fetch(
+		`/worlds/{{ world.id }}/state/json`,
+		{
+			method: `GET`,
+			headers: {"Content-Type": "application/json"},
+		}
+	);
+
+	let response_json = await response.json();
+
+	LAST_PLAYED_TD.innerHTML = response_json.last_played;
+	RUNNING_CONTAINER_TD.innerHTML = response_json.running_container_html;
+	RUN_BUTTON_DIV.innerHTML = response_json.run_button_html;
+	STATE_TD.innerHTML = response_json.state;
+
+	if(response_json.state == "running" || response_json.state == "offline")
+	{
+		clearInterval(UPDATE_STATE_INTERVAL);
+	}
+	if(response_json.state == "running")
+	{
+		GET_ONLINE_PLAYERS_INTERVAL = setInterval(get_online_players, 5000);
+		get_online_players();
+	}
+	if(response_json.state == "offline")
+	{
+		clearInterval(GET_ONLINE_PLAYERS_INTERVAL);
+	}
+}
+
+
+function edit_value(
+	element: HTMLElement,
+	original_value: string,
+	endpoint: string,
+	original_value_can_be_falsey: boolean=false
+): void
+{
+	// Prevent inputs within inputs
+	if(element.firstElementChild !== null)
+	{
+		return;
+	}
+
+	let input_string = `
+		<form
+			action="${endpoint}"
+			method="POST"
+			onsubmit="update_check(event, this, \`${original_value}\`, \`${element.innerHTML}\`);"
+		>
+			<input
+				id="edit_value-input"
+				name="edit_value-input"
+				onchange="this.parentElement.requestSubmit();"
+				onfocusout="this.parentElement.requestSubmit();"
+				value="${(original_value || original_value_can_be_falsey) ? original_value : ``}"
+			/>
+		</form>
+	`;
+	element.innerHTML = input_string;
+
+	document.getElementById("edit_value-input")!.focus();
+}
+
+
+function update_check(event: SubmitEvent, element: HTMLElement, original_value: string, original_HTML: string): void
+{
+	let new_value: string = (element.firstElementChild as HTMLInputElement).value;
+	if(new_value === original_value)
+	{
+		(element.parentElement as HTMLInputElement).innerHTML = original_HTML;
+		event.preventDefault();
+	}
+}
+
+
+
+let GET_ONLINE_PLAYERS_INTERVAL: ReturnType<typeof setInterval>;
+let UPDATE_STATE_INTERVAL: ReturnType<typeof setInterval>;
+if({{ world.state | tojson }} !== "offline" && {{ world.state | tojson }} !== "running")
+{
+	UPDATE_STATE_INTERVAL = setInterval(update_state, 1000);
+}
+else if({{ world.state | tojson }} === "running")
+{
+	get_online_players();
+	GET_ONLINE_PLAYERS_INTERVAL = setInterval(get_online_players, 5000);
+}
